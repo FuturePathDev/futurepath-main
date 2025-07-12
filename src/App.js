5f422e940597acd38f31581e084d9e80f4296fc8
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-12 11:06:14 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StudentDashboard from "./StudentDashboard";
+import StudentDashboard from "./StudentDashboard.jsx";
 
 function App() {
   return (
