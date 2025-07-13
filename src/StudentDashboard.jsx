208c3497cae39d29670098dc80a33f877ab16531
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-13 15:53:44 -0800

    Update StudentDashboard.jsx

--- src/StudentDashboard.jsx
+++ src/StudentDashboard.jsx
@@ -4,8 +4,7 @@ const StudentDashboard = () => {
     const [student, setStudent] = useState(null);
 
     useEffect(() => {
-        https://knntu7ft1l.execute-api.us-east-1.amazonaws.com/dev/StudentProfile
-
+        fetch("https://knntu7ft1l.execute-api.us-east-1.amazonaws.com/dev/StudentProfile")
             .then((res) => res.json())
             .then((data) => setStudent(data));
     }, []);
@@ -47,4 +46,3 @@ const StudentDashboard = () => {
 };
 
 export default StudentDashboard;
-
