5b84ae1de50df1b31b490938acbf200b9e3de11d
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-13 16:48:42 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import StudentDashboard from "./StudentDashboard";
+import UpdateProfile from "./UpdateProfile";
 
 function App() {
     return (
-        <div>
-            <StudentDashboard />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<StudentDashboard />} />
+                <Route path="/update-profile" element={<UpdateProfile />} />
+            </Routes>
+        </Router>
     );
 }
 
