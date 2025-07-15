13a69a929df0cd57ef4bbad3bf993c5b6532081a
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-15 00:32:05 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import CareerCharts from './components/CareerCharts';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StudentDashboard from "./StudentDashboard";
+import UpdateProfile from "./UpdateProfile";
+import CareerCharts from "./components/CareerCharts";
+import DashboardLayout from "./components/DashboardLayout";
 
+const student = {
+    name: "Jordan Taylor",
+    profile: {
+        grade: "10th",
+        school: "Jefferson High School",
+        district: "Salem-Keizer SD",
+        careerInterest: ["Software Developer", "Data Analyst"]
+    }
+};
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/career-charts" element={<CareerCharts />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
+                <Route
+                    path="/"
+                    element={
+                        <DashboardLayout student={student}>
+                            <StudentDashboard />
+                        </DashboardLayout>
+                    }
+                />
+                <Route
+                    path="/update-profile"
+                    element={
+                        <DashboardLayout student={student}>
+                            <UpdateProfile />
+                        </DashboardLayout>
+                    }
+                />
+                <Route
+                    path="/career-charts"
+                    element={
+                        <DashboardLayout student={student}>
+                            <CareerCharts />
+                        </DashboardLayout>
+                    }
+                />
             </Routes>
         </Router>
     );
 }
 
 export default App;
+
