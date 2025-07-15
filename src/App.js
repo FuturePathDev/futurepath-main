42e068b27d9d9f1cbb569fbc8aa542846f1a79e7
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-15 00:45:56 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StudentDashboard from "./StudentDashboard";
-import UpdateProfile from "./UpdateProfile";
-import CareerCharts from "./components/CareerCharts";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayout from "./components/DashboardLayout";
+import Dashboard from "./components/Dashboard";
+import CareerCharts from "./components/CareerCharts";
+import UpdateProfile from "./UpdateProfile";
 
 const student = {
     name: "Jordan Taylor",
     profile: {
         grade: "10th",
-        school: "Jefferson High School",
-        district: "Salem-Keizer SD",
-        careerInterest: ["Software Developer", "Data Analyst"]
-    }
+    },
 };
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <DashboardLayout student={student}>
-                            <StudentDashboard />
-                        </DashboardLayout>
-                    }
-                />
-                <Route
-                    path="/update-profile"
-                    element={
-                        <DashboardLayout student={student}>
-                            <UpdateProfile />
-                        </DashboardLayout>
-                    }
-                />
-                <Route
-                    path="/career-charts"
-                    element={
-                        <DashboardLayout student={student}>
-                            <CareerCharts />
-                        </DashboardLayout>
-                    }
-                />
-            </Routes>
+            <DashboardLayout student={student}>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/career-charts" element={<CareerCharts />} />
+                    <Route path="/update-profile" element={<UpdateProfile />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </DashboardLayout>
         </Router>
     );
 }
