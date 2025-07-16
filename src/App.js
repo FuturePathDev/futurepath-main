18e0189e0d161989d11fcef6f90a9cd6cbb23a9e
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-16 13:17:33 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import UpdateProfile from "./UpdateProfile";
-import CareerCharts from "./components/CareerCharts";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./components/DashboardLayout";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<DashboardLayout><Dashboard /></DashboardLayout>} />
-                <Route path="/update-profile" element={<DashboardLayout><UpdateProfile /></DashboardLayout>} />
-                <Route path="/career-charts" element={<DashboardLayout><CareerCharts /></DashboardLayout>} />
-                <Route path="*" element={<Navigate to="/" replace />} />
+                <Route path="/" element={<DashboardLayout />} />
             </Routes>
         </Router>
     );
 }
 
 export default App;
+
