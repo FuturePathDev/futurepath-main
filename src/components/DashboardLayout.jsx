89522eab0dbad73d4bea3517fba9ac2c4aede76d
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-17 14:17:02 -0800

    Update DashboardLayout.jsx

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import './DashboardLayout.css';
 
 const DashboardLayout = ({ studentInfo, children }) => {
     return (
-        <div className="flex">
+        <div className="flex min-h-screen font-raleway">
             <Sidebar />
-            <div className="flex-1 ml-64 min-h-screen bg-gradient-to-r from-blue-100 to-teal-100 p-8 font-raleway">
-                <div className="flex justify-end mb-8">
-                    <div className="text-right text-blue-900">
+            <div className="flex-1 p-8 bg-gradient-to-r from-blue-100 to-teal-100">
+                <div className="flex justify-end text-right text-blue-900 mb-6">
+                    <div>
                         <p className="font-bold">{studentInfo.name}</p>
                         <p>{studentInfo.grade}</p>
                         <p>{studentInfo.school}</p>
