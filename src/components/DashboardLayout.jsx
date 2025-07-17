c376212fe133fee1508f55587748202e25f84af1
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-16 18:42:06 -0800

    Update DashboardLayout.jsx

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,37 +1,38 @@
 import React from "react";
 import CareerCharts from "./CareerCharts";
-import LucrativeCareersChart from "./LucrativeCareersChart";
-import EsteemedSchoolsChart from "./EsteemedSchoolsChart";
-import SuggestedGoalsChart from "./SuggestedGoalsChart";
 import ProgressTracker from "./ProgressTracker";
+import EsteemedSchoolsChart from "./EsteemedSchoolsChart";
+import LucrativeCareersChart from "./LucrativeCareersChart";
+import SuggestedGoalsChart from "./SuggestedGoalsChart";
 
 const DashboardLayout = () => {
     return (
-        <div>
-            <div className="sidebar">
-                <h2>Menu</h2>
-                <ul>
-                    <li>Dashboard</li>
-                    <li>Career Charts</li>
-                    <li>Schools</li>
-                    <li>Goals</li>
-                    <li>Progress</li>
-                </ul>
-            </div>
-            <div className="content">
-                <div className="top-right-info">
-                    Jordan Taylor | 10th Grade | Jefferson High School
+        <div style={{ display: "flex", height: "100vh", background: "linear-gradient(to right, #a1c4fd, #c2e9fb)" }}>
+            <aside className="sidebar">
+                <h2>FuturePath</h2>
+                <nav>
+                    <a href="#" className="menu-item">Dashboard</a>
+                    <a href="#" className="menu-item">Careers</a>
+                    <a href="#" className="menu-item">Schools</a>
+                    <a href="#" className="menu-item">Goals</a>
+                    <a href="#" className="menu-item">Progress</a>
+                </nav>
+            </aside>
+            <main className="main-content" style={{ flex: 1, padding: "20px" }}>
+                <div className="student-info">
+                    <p>Jordan Taylor</p>
+                    <p>10th Grade</p>
+                    <p>Jefferson High School</p>
                 </div>
-                <h1>FuturePath Dashboard</h1>
+                <h1>Career Dashboard</h1>
                 <CareerCharts />
-                <LucrativeCareersChart />
                 <EsteemedSchoolsChart />
                 <SuggestedGoalsChart />
+                <LucrativeCareersChart />
                 <ProgressTracker />
-            </div>
+            </main>
         </div>
     );
 };
 
 export default DashboardLayout;
-
