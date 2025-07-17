52b496b8959c1c36781beb8ac772850e5e0a37e4
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-17 11:30:43 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import DashboardLayout from './components/DashboardLayout';
-import Careers from './components/Careers';
-import Schools from './components/Schools';
-import Goals from './components/Goals';
-import Resources from './components/Resources';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Careers from "./components/Careers";
+import Goals from "./components/Goals";
+import Schools from "./components/Schools";
+import Resources from "./components/Resources";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<DashboardLayout />} />
+                <Route path="/" element={<Dashboard />} />
                 <Route path="/careers" element={<Careers />} />
-                <Route path="/schools" element={<Schools />} />
                 <Route path="/goals" element={<Goals />} />
+                <Route path="/schools" element={<Schools />} />
                 <Route path="/resources" element={<Resources />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </Router>
     );
