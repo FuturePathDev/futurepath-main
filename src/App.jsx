8fd92c85ad4c5a86de028ddde1ed82f3381bda5c
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-17 20:56:20 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
 import ConfirmPage from './pages/ConfirmPage';
+import SignInPage from './pages/SignInPage';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<SignupPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/confirm" element={<ConfirmPage />} />
+        <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </Router>
   );
