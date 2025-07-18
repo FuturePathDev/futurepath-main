2c592df826e7eba2c64fa4497d6ae1fea27aa234
FuturePathDev <Jlavenderjanet@gmail.com>
2025-07-17 17:03:14 -0800

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
